8efe230b1dd5dc9abb20240ed95361fd95c2b648
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-04-11 14:40:58 +0200

    frontend: Update KubeObject to use selected clusters and pass cluster parameter in authorization methods
    Signed-off-by: Oleksandr Dubenko <oldubenko@microsoft.com>

--- frontend/src/lib/k8s/KubeObject.ts
+++ frontend/src/lib/k8s/KubeObject.ts
@@ -4,8 +4,8 @@ import React, { useMemo } from 'react';
 import { loadClusterSettings } from '../../helpers/clusterSettings';
 import { getCluster, getSelectedClusters } from '../cluster';
 import { createRouteURL } from '../router';
-import { getSelectedClusters, timeAgo } from '../util';
-import { useConnectApi } from '.';
+import { timeAgo } from '../util';
+import { useConnectApi, useSelectedClusters } from '.';
 import { RecursivePartial } from './api/v1/factories';
 import { useKubeObject } from './api/v2/hooks';
 import { makeListRequests, useKubeObjectList } from './api/v2/useKubeObjectList';
@@ -320,7 +320,7 @@ export class KubeObject<T extends KubeObjectInterface | KubeEvent = any> {
       refetchInterval?: number;
     } & QueryParameters = {}
   ) {
-    const fallbackClusters = useClusterGroup();
+    const fallbackClusters = useSelectedClusters();
 
     // Create requests for each cluster and namespace
     const requests = useMemo(() => {
@@ -488,7 +488,7 @@ export class KubeObject<T extends KubeObjectInterface | KubeEvent = any> {
    * @param reResourceAttrs The attributes describing this access request. See https://kubernetes.io/docs/reference/kubernetes-api/authorization-resources/self-subject-access-review-v1/#SelfSubjectAccessReviewSpec .
    * @returns The result of the access request.
    */
-  static async fetchAuthorization(reqResourseAttrs?: AuthRequestResourceAttrs) {
+  static async fetchAuthorization(reqResourseAttrs?: AuthRequestResourceAttrs, cluster?: string) {
     // @todo: We should get the API info from the API endpoint.
     const authApiVersions = ['v1', 'v1beta1'];
     for (let j = 0; j < authApiVersions.length; j++) {
@@ -504,7 +504,8 @@ export class KubeObject<T extends KubeObjectInterface | KubeEvent = any> {
               resourceAttributes: reqResourseAttrs,
             },
           },
-          false
+          false,
+          { cluster }
         );
       } catch (err) {
         // If this is the last attempt or the error is not 404, let it throw.
@@ -515,7 +516,11 @@ export class KubeObject<T extends KubeObjectInterface | KubeEvent = any> {
     }
   }
 
-  static async getAuthorization(verb: string, reqResourseAttrs?: AuthRequestResourceAttrs) {
+  static async getAuthorization(
+    verb: string,
+    reqResourseAttrs?: AuthRequestResourceAttrs,
+    cluster?: string
+  ) {
     const resourceAttrs: AuthRequestResourceAttrs = {
       verb,
       ...reqResourseAttrs,
@@ -530,7 +535,7 @@ export class KubeObject<T extends KubeObjectInterface | KubeEvent = any> {
     // If we already have the group and version, then we can make the request without
     // trying the API info, which may have several versions and thus be less optimal.
     if (!!resourceAttrs.group && !!resourceAttrs.version && !!resourceAttrs.resource) {
-      return this.fetchAuthorization(resourceAttrs);
+      return this.fetchAuthorization(resourceAttrs, cluster);
     }
 
     // If we don't have the group or version, then we have to try all of the
@@ -544,7 +549,7 @@ export class KubeObject<T extends KubeObjectInterface | KubeEvent = any> {
       let authResult;
 
       try {
-        authResult = await this.fetchAuthorization(attrs);
+        authResult = await this.fetchAuthorization(attrs, cluster);
       } catch (err) {
         // If this is the last attempt or the error is not 404, let it throw.
         if ((err as ApiError).status !== 404 || i === apiInfo.length - 1) {
@@ -584,7 +589,7 @@ export class KubeObject<T extends KubeObjectInterface | KubeEvent = any> {
       resourceAttrs['version'] = version;
     }
 
-    return this._class().getAuthorization(verb, resourceAttrs);
+    return this._class().getAuthorization(verb, resourceAttrs, this.cluster);
   }
 
   static getErrorMessage(err: ApiError | null) {
