38209f09861ec4f770cf9c98e919e0b1f1d55241
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-07-07 14:31:02 +0200

    frontend: DrawerModeSettings: Update settings for new Activities

--- frontend/src/components/App/Settings/DrawerModeSettings.tsx
+++ frontend/src/components/App/Settings/DrawerModeSettings.tsx
@@ -14,45 +14,150 @@
  * limitations under the License.
  */
 
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Icon } from '@iconify/react';
+import { alpha, Box, Button, useTheme } from '@mui/material';
+import React, { ReactNode } from 'react';
+import { Trans } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { setDetailDrawerEnabled } from '../../../redux/drawerModeSlice';
 import { useTypedSelector } from '../../../redux/hooks';
-import { TooltipIcon } from '../../common/Tooltip';
+
+function OverlayPreview({ variant }: { variant: 'full-page' | 'overlay' }) {
+  const theme = useTheme();
+  const size = '150px';
+
+  return (
+    <Box
+      sx={{
+        width: size,
+        height: size,
+        border: '1px solid',
+        borderColor: theme.palette.divider,
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius + 'px',
+        overflow: 'hidden',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '10%',
+          borderBottom: '1px solid',
+          borderColor: theme.palette.divider,
+        }}
+      ></Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '20%',
+          height: '90%',
+          top: '10%',
+          left: 0,
+          borderRight: '1px solid',
+          borderColor: theme.palette.divider,
+        }}
+      />
+      {variant === 'overlay' && (
+        <Box
+          sx={{
+            position: 'absolute',
+            background: theme.palette.background.muted,
+            width: '50%',
+            height: '90%',
+            top: '10%',
+            left: '50%',
+            border: '1px solid',
+            borderColor: theme.palette.divider,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: theme.shape.borderRadius + 'px',
+          }}
+        >
+          <Box sx={{ position: 'absolute', top: 0, right: 0, padding: '3px' }}>
+            <Icon icon="mdi:close" />
+          </Box>
+        </Box>
+      )}
+      {variant === 'full-page' && (
+        <Box
+          sx={{
+            position: 'absolute',
+            background: theme.palette.background.muted,
+            width: '80%',
+            height: '90%',
+            top: '10%',
+            left: '20%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              padding: '3px',
+            }}
+          >
+            <Icon icon="mdi:chevron-left" />
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
+}
+
+const OptionButton = ({
+  children,
+  active,
+  onClick,
+}: {
+  children: ReactNode;
+  active: boolean;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}) => (
+  <Button
+    aria-pressed={active}
+    onClick={onClick}
+    sx={theme => ({
+      display: 'flex',
+      flexDirection: 'column',
+      color: 'unset',
+      textTransform: 'none',
+      gap: 1,
+      border: '2px solid',
+      borderColor: active
+        ? alpha(theme.palette.action.active, theme.palette.action.activatedOpacity)
+        : 'transparent',
+    })}
+  >
+    {children}
+  </Button>
+);
 
 export default function DrawerModeSettings() {
   const dispatch = useDispatch();
-  const { t } = useTranslation('translation');
 
   const isDrawerEnabled = useTypedSelector(state => state.drawerMode.isDetailDrawerEnabled);
 
-  function drawerModeToggle() {
-    dispatch(setDetailDrawerEnabled(!isDrawerEnabled));
-  }
-
   return (
-    <FormControlLabel
-      control={
-        <Switch
-          checked={isDrawerEnabled}
-          onClick={drawerModeToggle}
-          name="drawerMode"
-          color="primary"
-        />
-      }
-      label={
-        <>
-          {t('translation|Overlay')}
-          <TooltipIcon>
-            {t(
-              'translation|Shows resource details in a pane overlaid on the list view instead of a full page.'
-            )}
-          </TooltipIcon>
-        </>
-      }
-    />
+    <Box sx={{ display: 'flex' }}>
+      <OptionButton active={isDrawerEnabled} onClick={() => dispatch(setDetailDrawerEnabled(true))}>
+        <OverlayPreview variant="overlay" />
+        <Trans>Window</Trans>
+      </OptionButton>
+      <OptionButton
+        active={!isDrawerEnabled}
+        onClick={() => dispatch(setDetailDrawerEnabled(false))}
+      >
+        <OverlayPreview variant="full-page" />
+        <Trans>Full page</Trans>
+      </OptionButton>
+    </Box>
   );
 }
