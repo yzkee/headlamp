vyncent-t/headlamp-workspace

frontend: TimezoneSelect: Add ARIA label for timezone select component

frontend/src/components/App/Settings/Settings.tsx
@@ -80,6 +80,7 @@ export default function Settings() {
   const sidebarLabel = 'sort-sidebar-label';
   const evictLabel = 'use-evict-label';
   const tableRowsLabel = 'rows-per-page-label';
+  const timezoneLabel = 'timezone-label';
 
   return (
     <SectionBox
@@ -125,9 +126,11 @@ export default function Settings() {
                 <TimezoneSelect
                   initialTimezone={selectedTimezone}
                   onChange={name => setSelectedTimezone(name)}
+                  nameLabelID={timezoneLabel}
                 />
               </Box>
             ),
+            nameID: timezoneLabel,
           },
           {
             name: t('translation|Sort sidebar items alphabetically'),

yzkee/headlamp

frontend/src/components/App/Settings/Settings.tsx
@@ -80,6 +80,7 @@ export default function Settings() {
   const sidebarLabelID = 'sort-sidebar-label';
   const evictLabelID = 'use-evict-label';
   const tableRowsLabelID = 'rows-per-page-label';
+  const timezoneLabelID = 'timezone-label';
 
   return (
     <SectionBox
@@ -125,9 +126,11 @@ export default function Settings() {
                 <TimezoneSelect
                   initialTimezone={selectedTimezone}
                   onChange={name => setSelectedTimezone(name)}
+                  nameLabelID={timezoneLabelID}
                 />
               </Box>
             ),
+            nameID: timezoneLabelID,
           },
           {
             name: t('translation|Sort sidebar items alphabetically'),