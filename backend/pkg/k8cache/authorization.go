e778439393c058dba443c88d307faa835b07d12d
Saurav Upadhyay <sauravup041103@gmail.com>
2025-08-27 17:44:30 +0000

    backend: k8cache/authorization: Add ClientSet for clientSet while authorizing user
    This adds GetClientSet which return *kubernetes.ClientSet and error which further used for creating
    SSAR requests to k8s server to authorize user. getClientSet uses kubeconfig.Context and
    authentication bearer token  which will help to create clientSet based on the user's
    identity.

---
+++ backend/pkg/k8cache/authorization.go
@@ -0,0 +1,81 @@
+// Copyright 2025 The Kubernetes Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package k8cache provides caching utilities for Kubernetes API responses.
+// It includes middleware for intercepting cluster API requests, generating
+// unique cache keys, storing and retrieving responses, and invalidating
+// entries when resources change. The package aims to reduce redundant
+// API calls, improve performance, and handle authorization gracefully
+// while maintaining consistency across multiple Kubernetes contexts.
+package k8cache
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/kubernetes-sigs/headlamp/backend/pkg/kubeconfig"
+	"github.com/kubernetes-sigs/headlamp/backend/pkg/logger"
+	"k8s.io/client-go/kubernetes"
+)
+
+type CachedClientSet struct {
+	clientset *kubernetes.Clientset
+	lastUsed  time.Time
+}
+
+var (
+	clientsetCache = make(map[string]*CachedClientSet)
+	mu             sync.Mutex
+)
+
+// GetClientSet return *kubernetes.ClientSet and error which further used for creating
+// SSAR requests to k8s server to authorize user. GetClientSet uses kubeconfig.Context and
+// authentication bearer token  which will help to create clientSet based on the user's
+// identity.
+func GetClientSet(k *kubeconfig.Context, token string) (*kubernetes.Clientset, error) {
+	contextKey := strings.Split(k.ClusterID, "+")
+	if len(contextKey) < 2 {
+		return nil, fmt.Errorf("unexpected ClusterID format in getClientSet: %q", k.ClusterID)
+	}
+
+	cacheKey := fmt.Sprintf("%s-%s", contextKey[1], token)
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	if cs, found := clientsetCache[cacheKey]; found {
+		now := time.Now()
+
+		if now.Sub(cs.lastUsed) > 10*time.Minute { // If the clientset was expired then delete
+			// the existing clientset resulting only fresh clientset.
+			delete(clientsetCache, cacheKey)
+			logger.Log(logger.LevelInfo, nil, nil, "clientset "+cacheKey+" was deleted")
+		} else {
+			return cs.clientset, nil // If the clientset is not expired then return directly.
+		}
+	}
+
+	cs, err := k.ClientSetWithToken(token)
+	if err != nil {
+		return nil, fmt.Errorf("error while creating clientset for key %s: %w", cacheKey, err)
+	}
+
+	clientsetCache[cacheKey] = &CachedClientSet{
+		clientset: cs,
+		lastUsed:  time.Now(),
+	}
+
+	return cs, nil
+}
---
+++ backend/pkg/k8cache/authorization_test.go
@@ -0,0 +1,106 @@
+// Copyright 2025 The Kubernetes Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8cache_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/kubernetes-sigs/headlamp/backend/pkg/k8cache"
+	"github.com/kubernetes-sigs/headlamp/backend/pkg/kubeconfig"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
+)
+
+type MockKubeConfig struct {
+	*kubeconfig.Context
+}
+
+func (k *MockKubeConfig) ClientSetWithToken(token string) (kubernetes.Interface, error) {
+	return fake.NewSimpleClientset(), nil
+}
+
+type MockClientConfig struct{}
+
+func (k *MockKubeConfig) ClientConfig() (clientcmd.ClientConfig, error) {
+	conf := api.Config{
+		Clusters: map[string]*api.Cluster{
+			k.KubeContext.Cluster: k.Cluster,
+		},
+		AuthInfos: map[string]*api.AuthInfo{
+			k.KubeContext.AuthInfo: k.AuthInfo,
+		},
+		Contexts: map[string]*api.Context{
+			k.Name: k.KubeContext,
+		},
+	}
+
+	return clientcmd.NewNonInteractiveClientConfig(conf, "kind-headlamp-admin", nil, nil), nil
+}
+
+func TestGetClientSet(t *testing.T) {
+	tests := []struct {
+		name          string
+		mockK         MockKubeConfig
+		token         string
+		clientSet     *kubernetes.Clientset
+		expectedError error
+	}{
+		{
+			name: "valid ClusterID returns cached clientset",
+			mockK: MockKubeConfig{
+				&kubeconfig.Context{
+					ClusterID:   "/home/user/.kubeconfig+kind-headlamp-admin",
+					Cluster:     &api.Cluster{Server: "https://example.com"},
+					AuthInfo:    &api.AuthInfo{Token: "abcdef"},
+					KubeContext: &api.Context{Cluster: "kind-headlamp-admin"},
+				},
+			},
+			token:         "token-1245",
+			expectedError: nil,
+		},
+		{
+			name: "return unexpected ClusterID format",
+			mockK: MockKubeConfig{
+				&kubeconfig.Context{
+					ClusterID:   "/home/user/.kubeconfig/kind-headlamp-admin",
+					Cluster:     &api.Cluster{Server: "https://example.com"},
+					AuthInfo:    &api.AuthInfo{Token: "abcdef"},
+					KubeContext: &api.Context{Cluster: "kind-headlamp-admin"},
+				},
+			},
+			token: "token-54321",
+			expectedError: fmt.Errorf("unexpected ClusterID format in getClientSet: " +
+				"\"/home/user/.kubeconfig/kind-headlamp-admin\""),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			cs, err := k8cache.GetClientSet(tc.mockK.Context, tc.token)
+			if tc.clientSet != nil { // It is difficult to compare the expected clientset with
+				// the returned clientSet as it return nested-struct inside the clientset which
+				// returns only memory references. To check whether the clientset was correct or
+				// not we can check by checking whether there was an error, so if there was an
+				// error clientset will be empty.
+				assert.NotEmpty(t, cs)
+				assert.NoError(t, err)
+			} else {
+				assert.Equal(t, tc.expectedError, err)
+			}
+		})
+	}
+}
