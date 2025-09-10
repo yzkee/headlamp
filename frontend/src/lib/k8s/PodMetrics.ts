bc7648240277d191df13c73f58ae08c4f19c2545
René Dudfield <renedudfield@microsoft.com>
2025-09-10 11:17:32 +0200

    frontend: lib/k8s/PodMetrics: Add type imports

--- frontend/src/lib/k8s/PodMetrics.ts
+++ frontend/src/lib/k8s/PodMetrics.ts
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-import { KubeObject, KubeObjectInterface } from './KubeObject';
+import type { KubeObjectInterface } from './KubeObject';
+import { KubeObject } from './KubeObject';
 
 interface KubePodMetrics extends KubeObjectInterface {
   timestamp: string;
