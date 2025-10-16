22de4dfb54942a7c2d236ad3f3d4ce04d0c77adb
Evangelos Skopelitis <eskopelitis@microsoft.com>
2025-10-16 09:00:32 -0400

    frontend: Add stories for NetworkPolicy
    This change adds storybook coverage of the NetworkPolicy component,
    which was recently updated.

---
+++ frontend/src/components/networkpolicy/List.stories.tsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2025 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Meta, StoryFn } from '@storybook/react';
+import { http, HttpResponse } from 'msw';
+import { TestContext } from '../../test';
+import { NetworkPolicyList } from './List';
+import { NETWORK_POLICY_ITEMS } from './storyHelper';
+
+export default {
+  title: 'NetworkPolicy/ListView',
+  component: NetworkPolicyList,
+  argTypes: {},
+  decorators: [
+    Story => {
+      return (
+        <TestContext>
+          <Story />
+        </TestContext>
+      );
+    },
+  ],
+} as Meta;
+
+const Template: StoryFn = () => {
+  return <NetworkPolicyList />;
+};
+
+export const Items = Template.bind({});
+Items.parameters = {
+  msw: {
+    handlers: {
+      story: [
+        http.get('http://localhost:4466/apis/networking.k8s.io/v1/networkpolicies', () =>
+          HttpResponse.json({
+            kind: 'NetworkPolicyList',
+            items: NETWORK_POLICY_ITEMS,
+            metadata: {},
+          })
+        ),
+      ],
+    },
+  },
+};
