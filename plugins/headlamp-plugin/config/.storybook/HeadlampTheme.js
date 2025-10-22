737d84651b2dd30fbf52f2a74b90b8b254a93e66
René Dudfield <renedudfield@microsoft.com>
2025-10-22 09:25:00 -0300

    docs: headlamp-plugin: Fix storybook theme logo and fonts
    This updates docs and storybook to use the new logo.

--- plugins/headlamp-plugin/config/.storybook/HeadlampTheme.js
+++ plugins/headlamp-plugin/config/.storybook/HeadlampTheme.js
@@ -2,9 +2,13 @@
 //  To workaround a bug at time of writing, where theme is not refreshed,
 //  you may need to `npm run storybook --no-manager-cache`
 import { create } from 'storybook/theming/create';
+import logoUrl from './headlamp_light.svg';
 
 export default create({
   base: 'light',
   brandTitle: 'Headlamp plugin storybook',
   brandUrl: 'https://headlamp.dev/docs/latest/development/plugins/functionality/#functionality',
+  brandImage: logoUrl,
+  fontBase: '"Overpass", sans-serif',
+  fontCode: '"Overpass Mono", monospace',
 });
