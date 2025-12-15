15d6d852453dc892db37c220dff3763c6aaa95da
René Dudfield <renedudfield@microsoft.com>
2025-12-15 18:06:01 -0300

    backend: plugins: Fix for skipping CHMOD events in plug watcher
    This was causing a loop in the frontend if the plugin had a chmod
    file in it on Mac with electron.

--- backend/pkg/plugins/plugins.go
+++ backend/pkg/plugins/plugins.go
@@ -377,7 +377,13 @@ func canSendRefresh(c cache.Cache[interface{}]) bool {
 func HandlePluginEvents(staticPluginDir, userPluginDir, pluginDir string,
 	notify <-chan string, cache cache.Cache[interface{}],
 ) {
-	for range notify {
+	for event := range notify {
+		// Skip CHMOD events to avoid looping on electron when user-plugins folder is watched
+		// these events are not relevant to see if plugins have changed.
+		if strings.HasSuffix(event, "CHMOD") {
+			continue
+		}
+
 		// Set the refresh signal only if we cannot send it. We prevent it here
 		// because we only want to send refresh signals that *happen after* we are
 		// allowed to send them.
