90bc21ad186ea8db5b3fef911873892cb2c3fdd8
aabhinavvvvvvv <abhinavguptaxia9213@gmail.com>
2026-04-26 18:22:49 +0000

    frontend: router: Fix hooks called in inline component function

--- frontend/src/lib/router/index.tsx
+++ frontend/src/lib/router/index.tsx
@@ -138,6 +138,18 @@ const LazyGraphView = React.lazy(() =>
   import('../../components/resourceMap/GraphView').then(it => ({ default: it.GraphView }))
 );
 
+function SettingsClusterRedirect() {
+  const cluster = useCluster();
+  const history = useHistory();
+
+  React.useEffect(() => {
+    history.replace(`/settings/cluster?c=${cluster}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <></>;
+}
+
 /** @private */
 const defaultRoutes: { [routeName: string]: Route } = {
   projectCreateYaml: {
@@ -882,20 +894,7 @@ const defaultRoutes: { [routeName: string]: Route } = {
     },
     useClusterURL: true,
     noAuthRequired: true,
-    component: () => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const cluster = useCluster();
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const history = useHistory();
-
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      React.useEffect(() => {
-        history.replace(`/settings/cluster?c=${cluster}`);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
-
-      return <></>;
-    },
+    component: SettingsClusterRedirect,
   },
   settingsClusterHomeContext: {
     path: '/settings/cluster',
