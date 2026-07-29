474e1b2ea3c1479f5aa170c90c93e7d1651faf9b
illume <illume@users.noreply.github.com>
2026-07-29 07:11:11 +0000

    chocolatey: Bump Headlamp version to 0.44.0
    Signed-off-by: illume <illume@users.noreply.github.com>

--- app/windows/chocolatey/tools/chocolateyinstall.ps1
+++ app/windows/chocolatey/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$headlampVersion = '0.43.0'
+$headlampVersion = '0.44.0'
 $url = "https://github.com/kubernetes-sigs/headlamp/releases/download/v${headlampVersion}/Headlamp-${headlampVersion}-win-x64.exe"
-$checksum = '922ca8b7e419c159c94eb196fa248fffab8f8caca0517e5e34f648c73c10ddaa'
+$checksum = '57038c256dab3936f7a0a2dc83794378f182bbd29dc749a6f02a110e6c93a4e3'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
