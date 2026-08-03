4b1e4d7578def67213fd898f29d79cec1633e614
René Dudfield <renedudfield@microsoft.com>
2026-08-03 15:27:38 +0200

    e2e: Storybook: Test CRD discovery mocks
    Exercise the preview worker in a real browser so unhandled Kubernetes discovery requests cannot silently regress stories.

---
+++ e2e-tests/storybook-tests/crdMocks.spec.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2026 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { expect, test } from '@playwright/test';
+
+const crdV1Url = 'http://localhost:4466/apis/apiextensions.k8s.io/v1/customresourcedefinitions';
+const crdV1beta1Url =
+  'http://localhost:4466/apis/apiextensions.k8s.io/v1beta1/customresourcedefinitions';
+
+test('Storybook mocks custom resource definition discovery', async ({ page }) => {
+  await page.goto('/iframe.html?id=common-actionbutton--basic&viewMode=story');
+  await expect(page.getByRole('button', { name: 'Some label' })).toBeVisible();
+
+  const result = await page.evaluate(
+    async ({ v1Url, v1beta1Url }) => {
+      const response = await fetch(v1Url);
+      let v1beta1Failed = false;
+
+      try {
+        await fetch(v1beta1Url);
+      } catch {
+        v1beta1Failed = true;
+      }
+
+      return {
+        status: response.status,
+        body: await response.json(),
+        v1beta1Failed,
+      };
+    },
+    { v1Url: crdV1Url, v1beta1Url: crdV1beta1Url }
+  );
+
+  expect(result).toEqual({
+    status: 200,
+    body: {
+      kind: 'CustomResourceDefinitionList',
+      apiVersion: 'apiextensions.k8s.io/v1',
+      metadata: {},
+      items: [],
+    },
+    v1beta1Failed: true,
+  });
+});
